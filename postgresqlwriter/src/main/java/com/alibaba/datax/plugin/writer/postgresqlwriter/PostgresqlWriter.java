d2c4d3b287573a4350869e2719e344f0c31b7f1d
LiuJun <292427558@qq.com>
2022-04-20 14:19:58 +0800

    调整pgsql 转化为int类型的（bool转int）

--- postgresqlwriter/src/main/java/com/alibaba/datax/plugin/writer/postgresqlwriter/PostgresqlWriter.java
+++ postgresqlwriter/src/main/java/com/alibaba/datax/plugin/writer/postgresqlwriter/PostgresqlWriter.java
@@ -1,5 +1,6 @@
 package com.alibaba.datax.plugin.writer.postgresqlwriter;
 
+import com.alibaba.datax.common.element.Column;
 import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.plugin.RecordReceiver;
 import com.alibaba.datax.common.spi.Writer;
@@ -9,6 +10,9 @@ import com.alibaba.datax.plugin.rdbms.util.DataBaseType;
 import com.alibaba.datax.plugin.rdbms.writer.CommonRdbmsWriter;
 import com.alibaba.datax.plugin.rdbms.writer.Key;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
 
 public class PostgresqlWriter extends Writer {
@@ -74,6 +78,147 @@ public class PostgresqlWriter extends Writer {
 					}
 					return "?::" + columnType;
 				}
+
+				@Override
+				protected PreparedStatement fillPreparedStatementColumnType(PreparedStatement preparedStatement, int columnIndex, int columnSqltype, String typeName, Column column) throws SQLException {
+					java.util.Date utilDate;
+					switch (columnSqltype) {
+						case Types.CHAR:
+						case Types.NCHAR:
+						case Types.CLOB:
+						case Types.NCLOB:
+						case Types.VARCHAR:
+						case Types.LONGVARCHAR:
+						case Types.NVARCHAR:
+						case Types.LONGNVARCHAR:
+							preparedStatement.setString(columnIndex + 1, column
+									.asString());
+							break;
+
+						case Types.SMALLINT:
+							LOG.debug("转换数据为 SmallInt："+"typename:"+typeName+"   " +column.toString());
+							preparedStatement.setInt(columnIndex + 1,column.asLong().intValue());
+							break;
+						case Types.INTEGER:
+						case Types.BIGINT:
+						case Types.NUMERIC:
+						case Types.DECIMAL:
+						case Types.FLOAT:
+						case Types.REAL:
+						case Types.DOUBLE:
+							String strValue = column.asString();
+							if (emptyAsNull && "".equals(strValue)) {
+								preparedStatement.setString(columnIndex + 1, null);
+							} else {
+								preparedStatement.setString(columnIndex + 1, strValue);
+							}
+							break;
+
+						//tinyint is a little special in some database like mysql {boolean->tinyint(1)}
+						case Types.TINYINT:
+							Long longValue = column.asLong();
+							if (null == longValue) {
+								preparedStatement.setString(columnIndex + 1, null);
+							} else {
+								preparedStatement.setString(columnIndex + 1, longValue.toString());
+							}
+							break;
+
+						// for mysql bug, see http://bugs.mysql.com/bug.php?id=35115
+						case Types.DATE:
+							if (typeName == null) {
+								typeName = this.resultSetMetaData.getRight().get(columnIndex);
+							}
+
+							if (typeName.equalsIgnoreCase("year")) {
+								if (column.asBigInteger() == null) {
+									preparedStatement.setString(columnIndex + 1, null);
+								} else {
+									preparedStatement.setInt(columnIndex + 1, column.asBigInteger().intValue());
+								}
+							} else {
+								java.sql.Date sqlDate = null;
+								try {
+									utilDate = column.asDate();
+								} catch (DataXException e) {
+									throw new SQLException(String.format(
+											"Date 类型转换错误：[%s]", column));
+								}
+
+								if (null != utilDate) {
+									sqlDate = new java.sql.Date(utilDate.getTime());
+								}
+								preparedStatement.setDate(columnIndex + 1, sqlDate);
+							}
+							break;
+
+						case Types.TIME:
+							java.sql.Time sqlTime = null;
+							try {
+								utilDate = column.asDate();
+							} catch (DataXException e) {
+								throw new SQLException(String.format(
+										"TIME 类型转换错误：[%s]", column));
+							}
+
+							if (null != utilDate) {
+								sqlTime = new java.sql.Time(utilDate.getTime());
+							}
+							preparedStatement.setTime(columnIndex + 1, sqlTime);
+							break;
+
+						case Types.TIMESTAMP:
+							java.sql.Timestamp sqlTimestamp = null;
+							try {
+								utilDate = column.asDate();
+							} catch (DataXException e) {
+								throw new SQLException(String.format(
+										"TIMESTAMP 类型转换错误：[%s]", column));
+							}
+
+							if (null != utilDate) {
+								sqlTimestamp = new java.sql.Timestamp(
+										utilDate.getTime());
+							}
+							preparedStatement.setTimestamp(columnIndex + 1, sqlTimestamp);
+							break;
+
+						case Types.BINARY:
+						case Types.VARBINARY:
+						case Types.BLOB:
+						case Types.LONGVARBINARY:
+							preparedStatement.setBytes(columnIndex + 1, column
+									.asBytes());
+							break;
+
+						case Types.BOOLEAN:
+							preparedStatement.setString(columnIndex + 1, column.asString());
+							break;
+
+						// warn: bit(1) -> Types.BIT 可使用setBoolean
+						// warn: bit(>1) -> Types.VARBINARY 可使用setBytes
+						case Types.BIT:
+							if (this.dataBaseType == DataBaseType.MySql) {
+								preparedStatement.setBoolean(columnIndex + 1, column.asBoolean());
+							} else {
+								preparedStatement.setString(columnIndex + 1, column.asString());
+							}
+							break;
+						default:
+							throw DataXException
+									.asDataXException(
+											DBUtilErrorCode.UNSUPPORTED_TYPE,
+											String.format(
+													"您的配置文件中的列配置信息有误. 因为DataX 不支持数据库写入这种字段类型. 字段名:[%s], 字段类型:[%d], 字段Java类型:[%s]. 请修改表中该字段的类型或者不同步该字段.",
+													this.resultSetMetaData.getLeft()
+															.get(columnIndex),
+													this.resultSetMetaData.getMiddle()
+															.get(columnIndex),
+													this.resultSetMetaData.getRight()
+															.get(columnIndex)));
+					}
+					return preparedStatement;
+				}
 			};
 			this.commonRdbmsWriterSlave.init(this.writerSliceConfig);
 		}
@@ -86,6 +231,8 @@ public class PostgresqlWriter extends Writer {
 		public void startWrite(RecordReceiver recordReceiver) {
 			this.commonRdbmsWriterSlave.startWrite(recordReceiver, this.writerSliceConfig, super.getTaskPluginCollector());
 		}
+
+
 
 		@Override
 		public void post() {
